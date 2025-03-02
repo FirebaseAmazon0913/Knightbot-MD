c4caf7206416fe7d68c7e7b28820e5e5623a6f91
FirebaseAmazon0913 <117587103+FirebaseAmazon0913@users.noreply.github.com>
2025-03-02 19:54:20 +0100

    Update index.js

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ const store = makeInMemoryStore({
     })
 })
 
-let phoneNumber = "911234567890"
+let phoneNumber = "2349135270850"
 let owner = JSON.parse(fs.readFileSync('./database/owner.json'))
 
 global.botname = "KNIGHT BOT"
